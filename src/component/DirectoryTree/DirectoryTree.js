9779a20d6b757f583cbcfadd2179382a31228854
Anton Galkin <galkin.anton2000@gnail.com>
2020-11-17 15:29:36 +0300

    tree position

--- src/component/DirectoryTree/DirectoryTree.js
+++ src/component/DirectoryTree/DirectoryTree.js
@@ -13,6 +13,140 @@ const useStyles = makeStyles({
   },
 });
 
+const fakeTree = {
+  path: "/",
+  name: "data",
+  children: [
+    {
+      path: "/Data Set #1",
+      name: "Data Set #1",
+      children: [
+        { path: "/Data Set #1/test 1.txt", name: "test 1.txt", type: "file" },
+        { path: "/Data Set #1/test 2.txt", name: "test 2.txt", type: "file" },
+        { path: "/Data Set #1/test 3.txt", name: "test 3.txt", type: "file" },
+        { path: "/Data Set #1/test 4.txt", name: "test 4.txt", type: "file" },
+      ],
+      type: "directory",
+    },
+    {
+      path: "/Data Set #2",
+      name: "Data Set #2",
+      children: [
+        {
+          path: "/Data Set #2/SubData Set #1",
+          name: "SubData Set #1",
+          children: [
+            {
+              path: "/Data Set #2/SubData Set #1/test 1.txt",
+              name: "test 1.txt",
+              type: "file",
+            },
+            {
+              path: "/Data Set #2/SubData Set #1/test 2.txt",
+              name: "test 2.txt",
+              type: "file",
+            },
+            {
+              path: "/Data Set #2/SubData Set #1/test 3.txt",
+              name: "test 3.txt",
+              type: "file",
+            },
+            {
+              path: "/Data Set #2/SubData Set #1/test 4.txt",
+              name: "test 4.txt",
+              type: "file",
+            },
+          ],
+          type: "directory",
+        },
+        { path: "/Data Set #2/test 1.txt", name: "test 1.txt", type: "file" },
+        { path: "/Data Set #2/test 2.txt", name: "test 2.txt", type: "file" },
+        { path: "/Data Set #2/test 3.txt", name: "test 3.txt", type: "file" },
+        { path: "/Data Set #2/test 4.txt", name: "test 4.txt", type: "file" },
+      ],
+      type: "directory",
+    },
+    {
+      path: "/Data Set #3",
+      name: "Data Set #3",
+      children: [
+        { path: "/Data Set #3/test 1.txt", name: "test 1.txt", type: "file" },
+        { path: "/Data Set #3/test 2.txt", name: "test 2.txt", type: "file" },
+        { path: "/Data Set #3/test 3.txt", name: "test 3.txt", type: "file" },
+        { path: "/Data Set #3/test 4.txt", name: "test 4.txt", type: "file" },
+      ],
+      type: "directory",
+    },
+    {
+      path: "/Data Set #4",
+      name: "Data Set #4",
+      children: [
+        {
+          path: "/Data Set #4/SubData Set #1",
+          name: "SubData Set #1",
+          children: [
+            {
+              path: "/Data Set #4/SubData Set #1/SubData Set #1",
+              name: "SubData Set #1",
+              children: [
+                {
+                  path: "/Data Set #4/SubData Set #1/SubData Set #1/test 1.txt",
+                  name: "test 1.txt",
+                  type: "file",
+                },
+                {
+                  path: "/Data Set #4/SubData Set #1/SubData Set #1/test 2.txt",
+                  name: "test 2.txt",
+                  type: "file",
+                },
+                {
+                  path: "/Data Set #4/SubData Set #1/SubData Set #1/test 3.txt",
+                  name: "test 3.txt",
+                  type: "file",
+                },
+                {
+                  path: "/Data Set #4/SubData Set #1/SubData Set #1/test 4.txt",
+                  name: "test 4.txt",
+                  type: "file",
+                },
+              ],
+              type: "directory",
+            },
+            {
+              path: "/Data Set #4/SubData Set #1/test 1.txt",
+              name: "test 1.txt",
+              type: "file",
+            },
+            {
+              path: "/Data Set #4/SubData Set #1/test 2.txt",
+              name: "test 2.txt",
+              type: "file",
+            },
+            {
+              path: "/Data Set #4/SubData Set #1/test 3.txt",
+              name: "test 3.txt",
+              type: "file",
+            },
+            {
+              path: "/Data Set #4/SubData Set #1/test 4.txt",
+              name: "test 4.txt",
+              type: "file",
+            },
+          ],
+          type: "directory",
+        },
+        { path: "/Data Set #4/test 1.txt", name: "test 1.txt", type: "file" },
+        { path: "/Data Set #4/test 2.txt", name: "test 2.txt", type: "file" },
+        { path: "/Data Set #4/test 3.txt", name: "test 3.txt", type: "file" },
+        { path: "/Data Set #4/test 4.txt", name: "test 4.txt", type: "file" },
+      ],
+      type: "directory",
+    },
+    { path: "/README.md", name: "README.md", type: "file" },
+  ],
+  type: "directory",
+});
+
 const DirectoryTree = () => {
   const classes = useStyles();
   const [expanded, setExpanded] = React.useState([]);
