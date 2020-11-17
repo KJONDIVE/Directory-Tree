1ce93b02d227bef967b1db4566a48478e966e8f7
Anton Galkin <galkin.anton2000@gnail.com>
2020-11-17 14:55:53 +0300

    +directory tree template

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
+import DirectoryTree from "./component/DirectoryTree/DirectoryTree";
+
 function App() {
   return (
     <div className="App">
       <h1>Directory Tree Output</h1>
+      <DirectoryTree />
     </div>
   );
 }
